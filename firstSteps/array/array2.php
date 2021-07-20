cba4f6fb553cbb5eca01286f4a7a1fb6610110cf
Frklcedo <franklinmacedo5115@gmail.com>
2021-07-20 09:08:16 -0300

    valaquenta

--- firstSteps/array/array2.php
+++ firstSteps/array/array2.php
@@ -4,22 +4,43 @@ $vector = array();
 
 array_push($vector, array(
     'name'=>'Ilúvatar',
-    'call'=>'Eru'
+    'call'=>'Eru',
+    'being'=>'Unus'
 ));
 
 array_push($vector, array(
     'name'=>'Melkor',
-    'call'=>'Desolation'
+    'call'=>'Lord of chaos and desolation',
+    'being'=>'Former Valar'
 ));
 
-array_push($vector, array('name'=>'Manwe','call'=>'Winds'
-));
+array_push($vector, array('name'=>'Manwe','call'=>'Lord of the winds','being'=>'Valar'));
 
-array_push($vector, array('name'=>'Varda','call'=>'Stars'
-));
+array_push($vector, array('name'=>'Varda','call'=>'Lady of the stars','being'=>'Valier'));
 
-array_push($vector, array('name'=>'Ulmo','call'=>'Seas'
-));
+array_push($vector, array('name'=>'Ulmo','call'=>'Lord of the waters','being'=>'Valar'));
+
+array_push($vector, array('name'=>'Aule','call'=>'Lord of the land','being'=>'Valar'));
+
+array_push($vector, array('name'=>'Yavanna','call'=>'Giver of Fruits','being'=>'Valier'));
+
+array_push($vector, array('name'=>'Námo','call'=>'Lord of Mandos, the house of the dead','being'=>'Valar'));
+
+array_push($vector, array('name'=>'Vaire','call'=>'Weaver','being'=>'Valier'));
+
+array_push($vector, array('name'=>'Irmo','call'=>'Lord of Lórien, dreams and visions','being'=>'Valar'));
+
+array_push($vector, array('name'=>'Este','call'=>'The kind','being'=>'Valier'));
+
+array_push($vector, array('name'=>'Nienna','call'=>'Lady of sadness and sorrow','being'=>'Valier'));
+
+array_push($vector, array('name'=>'Tulkas','call'=>'Valient heart','being'=>'Valar'));
+
+array_push($vector, array('name'=>'Nessa','call'=>'The swift','being'=>'Valier'));
+
+array_push($vector, array('name'=>'Orome','call'=>'The huntsman','being'=>'Valar'));
+
+array_push($vector, array('name'=>'Vána','call'=>'The ever-young','being'=>'Valier'));
 
 print_r($vector);
 
