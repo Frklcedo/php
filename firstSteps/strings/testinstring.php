eb314eea31e8d5238fd0439a3e25710653040c68
Frklcedo <franklinmacedo5115@gmail.com>
2021-08-05 20:58:30 -0300

    testing php simple strings

---
+++ firstSteps/strings/testinstring.php
@@ -0,0 +1,12 @@
+<?php
+
+$char='variables';
+
+echo 'This will all be printed as a string\n <br>'; //html still works 
+
+echo 'I can say with single quotes (\') and (\\) <br>';
+
+echo "With \ndouble quotes,\ti can use some \vextra characters and use $char<br>";
+
+echo "\$char = '$char'";
+
