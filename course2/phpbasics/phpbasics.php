f4b5e853df37bde248dfe93bc6b881d50c7c197e
Frklcedo <franklinmacedo5115@gmail.com>
2022-06-22 15:26:34 -0300

    booleans, ints and floats;

--- course2/phpbasics/phpbasics.php
+++ course2/phpbasics/phpbasics.php
@@ -59,3 +59,53 @@
         var_dump($isBool);
     }
     echo BR;
+
+    // int
+    $x = 42;
+    echo "basic int = $x" . BR;
+
+    $x = 0x2a;
+    echo "hexadecimal int = $x" . BR;
+
+    $x = 052;
+    echo "octonumber int = $x" . BR;
+
+    $x = 0b101010;
+    echo "binary int = $x" . BR;
+
+    $x = (int)'12cjas';
+    echo "string to int ('12cjas')= $x" . BR;
+    $x = (int)'teste';
+    echo "string to int ('teste')= $x" . BR;
+
+    // float
+
+    $x = 3.5;
+    var_dump($x);
+    echo BR;
+
+    $x = 3.5e3;
+    echo $x . BR;
+    $x = 3.5e-3;
+    echo $x . BR;
+
+    // errors on floats -some-functions
+    $x = 0.1 + 0.7;
+    echo $x . BR;
+    $x *= 10;
+    echo $x . BR;
+    $x = floor($x) ;
+    echo $x . BR;
+    $x = ceil((0.1 + 0.2) * 10) ;
+    echo $x . BR;
+
+
+    // NaN and INF
+    $x = PHP_FLOAT_MAX * 2;
+    var_dump(is_infinite($x));
+    var_dump(is_finite($x));
+    var_dump($x);
+    echo BR;
+    $x = log(-1);
+    var_dump(is_nan($x));
+    var_dump($x);
